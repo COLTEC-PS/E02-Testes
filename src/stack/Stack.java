61b583358e52dd1e206d659937ae4e91fce58a8d
Frederico Teixeira Azevedo <2020955533@teiacoltec.org>
2021-07-05 08:15:09 -0300

    First commit
    Commit 01: Classe Stack

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -1,6 +1,7 @@
 package stack;
 
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 import java.util.List;
 
 public class Stack<Trem> {
@@ -18,12 +19,14 @@ public class Stack<Trem> {
         this.topo++;
     }
 
-    public Trem desempilhar() throws Exception {
+    public Trem desempilhar() throws EmptyStackException {
         if(this.estaVazia()) {
-            return null;
+            throw new EmptyStackException();
+        } else {
+            Trem trem = vetor.remove(topo-1);
+            topo --;
+            return trem;
         }
-
-        return this.vetor.get(this.topo);
     }
 
     public boolean estaVazia() {
--- src/test/StackTest.java
+++ src/test/StackTest.java
@@ -7,4 +7,47 @@ import static org.junit.Assert.*;
 
 public class StackTest {
 
+    @Test
+    public void testEmptyStack() {
+        Stack<Integer> stack = new Stack();
+        boolean empty = stack.estaVazia();
+        assertTrue(empty);
+    }
+
+    @Test
+    public void testNotEmptyStack() {
+        Stack<Integer> stack = new Stack();
+        stack.empilhar(10);
+        assertFalse(stack.estaVazia());
+    }
+
+    @Test
+    public void testSizeStack() {
+        Stack<Integer> stack = new Stack();
+        stack.empilhar(10);
+        stack.empilhar(20);
+        stack.empilhar(30);
+        int size = stack.tamanho();
+        assertEquals(3,size);
+    }
+
+    @Test
+    public void testPushPopStack() {
+        Stack<Integer> stack = new Stack();
+        stack.empilhar(10);
+        stack.empilhar(20);
+        stack.empilhar(30);
+        int result = stack.desempilhar();
+        result = stack.desempilhar();
+        assertEquals(20,result);
+    }
+
+    @Test(expected = java.util.EmptyStackException.class)
+    public void testEmptyStackException() {
+        Stack<Integer> stack = new Stack();
+        stack.empilhar(10);
+        int result = stack.desempilhar();
+        result = stack.desempilhar();
+    }
+
 }
