27bd144cccf37488da35157085cb4acbcae0aae3
Fernanda de Oliveira Carvalho <nandaoc@pop-os.localdomain>
2021-08-15 00:57:05 -0300

    implementando métodos testados na classe QueueTest

--- src/queue/Queue.java
+++ src/queue/Queue.java
@@ -1,6 +1,28 @@
 package queue;
 
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Queue<Trem> {
 
+    private LinkedList<Trem> fila;
+
+    public void enfileirar(Trem x){
+        this.fila.add(x);
+    }
+
+    public boolean estaVazia() {
+        return this.fila.isEmpty();
+    }
+
+    public int tamanho() {
+        return this.fila.size();
+    }
+
+    public int desenfileirar(){
+        return (int) this.fila.remove();
+    }
 
 }
